eee00501991d579e157b3c36c0f22507e81dbdb5
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-06-20 13:20:24 +0700

    Add DocBlock on 'capitalize' helper

--- src/helpers/formatter.js
+++ src/helpers/formatter.js
@@ -1,2 +1,8 @@
+/**
+ * Make capital string
+ *
+ * @param {string} words String you want to capitalize
+ * @returns {string} Capitalized string
+ */
 export const capitalize = words =>
   words.charAt(0).toUpperCase() + words.substr(1);
