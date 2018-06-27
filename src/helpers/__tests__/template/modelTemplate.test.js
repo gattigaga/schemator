7b1e9e5d1dff4c009a267864ce885fa0b8618498
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-06-27 22:21:52 +0700

    Fix modelTemplate test

--- src/helpers/__tests__/template/modelTemplate.test.js
+++ src/helpers/__tests__/template/modelTemplate.test.js
@@ -2,29 +2,9 @@ import { modelTemplate } from "../../template";
 
 describe("modelTemplate()", () => {
   it("should returns created model", () => {
-    const expected = `
-    <?php
-
-    namespace App\\Models;
-    
-    use Illuminate\\Database\\Eloquent\\Model;
-    
-    class User extends Model
-    {
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array
-         */
-        protected $fillable = [
-            'username',
-            'email',
-            'fullname',
-        ];
-    }`;
     const fields = ["username", "email", "fullname"];
     const result = modelTemplate("User", fields);
 
-    expect(result).toEqual(expected);
+    expect(result).toMatchSnapshot();
   });
 });
