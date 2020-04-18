d3d0e6be00b781e48bc4b71b5cead37ec43e2976
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2020-04-18 23:35:26 +0700

    Refactor TableHeader component

--- src/components/presentational/TableHeader.js
+++ src/components/presentational/TableHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
@@ -15,39 +15,27 @@ const Header = styled.div`
   cursor: pointer;
 `;
 
-export const Caption = styled.input`
+const Caption = styled.input`
   color: white;
   border: 0px;
   width: 50%;
   outline: none;
   background: none;
   font-size: 12px;
-  font-family: Roboto;
+  font-family: "Roboto";
   font-weight: bold;
   text-align: center;
 `;
 
-class TableHeader extends Component {
-  shouldComponentUpdate(nextProps) {
-    const { caption } = this.props;
-
-    return nextProps.caption !== caption;
-  }
-
-  render() {
-    const { caption, onChangeCaption } = this.props;
-
-    return (
-      <Header>
-        <Caption type="text" value={caption} onChange={onChangeCaption} />
-      </Header>
-    );
-  }
-}
+const TableHeader = ({ caption, onChangeCaption }) => (
+  <Header>
+    <Caption type="text" value={caption} onChange={onChangeCaption} />
+  </Header>
+);
 
 TableHeader.propTypes = {
   caption: PropTypes.string,
-  onChangeCaption: PropTypes.func
+  onChangeCaption: PropTypes.func,
 };
 
 export default TableHeader;
